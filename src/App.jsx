3943bc9b2e6c0554a372beb78c99349f4c301c56
kiryhanchiiiik <162802442+kiryhanchiiiik@users.noreply.github.com>
2025-05-06 20:14:12 +0300

    contact section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import css from "./App.module.scss";
 import About from "./components/About/About";
 import Certification from "./components/Certification/Certification";
+import Contact from "./components/Contact/Contact";
 import Description from "./components/Description/Description";
 import Header from "./components/Header/Header";
 import Skills from "./components/Skills/Skills";
@@ -15,6 +16,7 @@ function App() {
       <Skills />
       <Work />
       <Certification />
+      <Contact />
     </div>
   );
 }
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,87 @@
+import css from "./Contact.module.scss";
+import sprite from "../../img/sprite.svg";
+
+const Contact = () => {
+  return (
+    <section className={css.contact}>
+      <div className={css.inner}>
+        <div className={css.contactText}>
+          <p>Get in touch</p>
+        </div>
+        <p className={css.contactDesc}>
+          My contacts – I'm always open to job opportunities and new
+          collaborations.
+        </p>
+        <address className={css.contacts}>
+          <div className={css.contactsLinks}>
+            <svg width={24} height={24}>
+              <use href={`${sprite}#post`}></use>
+            </svg>
+            <a
+              className={css.contactLink}
+              href="mailto:kirillsabelkijk@gmail.com"
+            >
+              kirillsabelkijk@gmail.com
+            </a>
+            <svg width={24} height={24}>
+              <use href={`${sprite}#copy`}></use>
+            </svg>
+          </div>
+          <div className={css.contactsLinks}>
+            <svg width={24} height={24}>
+              <use href={`${sprite}#phone`}></use>
+            </svg>
+            <a className={css.contactLink} href="tel:+37068425972">
+              +370 684 25 972
+            </a>
+            <svg width={24} height={24}>
+              <use href={`${sprite}#copy`}></use>
+            </svg>
+          </div>
+        </address>
+        <p>You may also find me on these platforms!</p>
+        <div className={css.socials}>
+          <a
+            className={css.link}
+            href="https://www.linkedin.com/in/kyryl-shchabelskyi/"
+          >
+            <svg className={css.svg} width={24} height={24}>
+              <use href={`${sprite}#linkedin`}></use>
+            </svg>
+          </a>
+          <a
+            className={css.link}
+            href="https://github.com/kiryhanchiiiik?tab=repositories"
+          >
+            <svg className={css.svg} width={24} height={24}>
+              <use href={`${sprite}#github`}></use>
+            </svg>
+          </a>
+          <a
+            className={css.link}
+            href="https://discord.com/users/428491636889223189"
+          >
+            <svg className={css.svg} width={24} height={24}>
+              <use href={`${sprite}#discord`}></use>
+            </svg>
+          </a>
+          <a
+            className={css.link}
+            href="https://www.instagram.com/kiryhanchiiiik/"
+          >
+            <svg className={css.svg} width={24} height={24}>
+              <use href={`${sprite}#instagram`}></use>
+            </svg>
+          </a>
+          <a className={css.link} href="https://t.me/shchabelskyii">
+            <svg className={css.svg} width={24} height={24}>
+              <use href={`${sprite}#telegram`}></use>
+            </svg>
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
