34dd30290ce1ee201a56d7bff759d2bc42b1fae0
kiryhanchiiiik <162802442+kiryhanchiiiik@users.noreply.github.com>
2025-05-08 15:29:40 +0300

    add header for desktop

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -19,14 +19,69 @@ const Header = () => {
         <a className={css.logo} href="#">
           &lt;kiryhanchik /&gt;
         </a>
-        <button type="button" onClick={toggleMenu}>
+
+        {/* Навигация для десктопа */}
+        <nav className={css.desktopNav}>
+          <ul className={css.list}>
+            <li>
+              <Link to="about" smooth offset={-20} duration={600}>
+                About
+              </Link>
+            </li>
+            <li>
+              <Link to="skills" smooth offset={-20} duration={600}>
+                Skills
+              </Link>
+            </li>
+            <li>
+              <Link to="work" smooth offset={-20} duration={600}>
+                Work
+              </Link>
+            </li>
+            <li>
+              <Link to="certification" smooth offset={-20} duration={600}>
+                Certification
+              </Link>
+            </li>
+            <li>
+              <Link to="contact" smooth duration={600}>
+                Contacts
+              </Link>
+            </li>
+            <li>
+              <span className={css.span}></span>
+            </li>
+          </ul>
+
+          <div className={css.btnInnerDesk}>
+            <div className={css.themeContainer}>
+              <button type="button">
+                <svg width={24} height={24}>
+                  <use href={`${sprite}#moon`}></use>
+                </svg>
+              </button>
+            </div>
+            <button className={css.cvBtn} type="button">
+              <a
+                href="../../../public/cv/Kyryl_Shchabelskyi_FullStackDeveloper.pdf"
+                download
+              >
+                Download CV
+              </a>
+            </button>
+          </div>
+        </nav>
+
+        {/* Бургер-кнопка для мобилки */}
+        <button className={css.openBtn} type="button" onClick={toggleMenu}>
           <svg width={30} height={30}>
             <use href={`${sprite}#${isMenuOpen ? "cross" : "burger"}`} />
           </svg>
         </button>
       </div>
 
-      <nav className={`${css.menu} ${isMenuOpen ? css.open : ""}`}>
+      {/* Мобильное меню */}
+      <nav className={`${css.mobileMenu} ${isMenuOpen ? css.open : ""}`}>
         <ul className={css.list}>
           <li>
             <Link
@@ -73,12 +128,18 @@ const Header = () => {
             </Link>
           </li>
           <li>
-            <Link to="contact" smooth={true} duration={600} onClick={closeMenu}>
+            <Link
+              to="contact"
+              smooth={true}
+              offset={-20}
+              duration={600}
+              onClick={closeMenu}
+            >
               Contacts
             </Link>
           </li>
         </ul>
-        <div className={css.btnInner}>
+        <div className={css.btnInnerMobile}>
           <div className={css.themeContainer}>
             <p>Switch Theme</p>
             <button type="button">
