4ace0743a8e6047ed5f188fcee1060813d4dd86a
kiryhanchiiiik <162802442+kiryhanchiiiik@users.noreply.github.com>
2025-05-12 15:14:59 +0300

    add hover with transition to skills section

--- src/components/TechList/TechList.jsx
+++ src/components/TechList/TechList.jsx
@@ -6,63 +6,83 @@ const TechList = () => {
   return (
     <ul className={css.techList}>
       <li>
-        <svg width={64} height={64}>
-          <use href={`${sprite}#javascript`}></use>
-        </svg>
+        <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript">
+          <svg width={64} height={64}>
+            <use href={`${sprite}#javascript`}></use>
+          </svg>
+        </a>
         <p className={css.techName}>Javascript</p>
       </li>
       <li>
-        <svg width={64} height={64}>
-          <use href={`${sprite}#typescript`}></use>
-        </svg>
+        <a href="https://www.w3schools.com/typescript/typescript_intro.php">
+          <svg width={64} height={64}>
+            <use href={`${sprite}#typescript`}></use>
+          </svg>
+        </a>
         <p className={css.techName}>Typescript</p>
       </li>
       <li>
-        <svg width={64} height={64}>
-          <use href={`${sprite}#react`}></use>
-        </svg>
+        <a href="https://react.dev/">
+          <svg width={64} height={64}>
+            <use href={`${sprite}#react`}></use>
+          </svg>
+        </a>
         <p className={css.techName}>React</p>
       </li>
       <li>
-        <svg width={64} height={64}>
-          <use href={`${sprite}#nodejs`}></use>
-        </svg>
+        <a href="https://nodejs.org/en">
+          <svg width={64} height={64}>
+            <use href={`${sprite}#nodejs`}></use>
+          </svg>
+        </a>
         <p className={css.techName}>Node.js</p>
       </li>
       <li>
-        <svg width={64} height={64}>
-          <use href={`${sprite}#express`}></use>
-        </svg>
+        <a href="https://expressjs.com/">
+          <svg width={64} height={64}>
+            <use href={`${sprite}#express`}></use>
+          </svg>
+        </a>
         <p className={css.techName}>Express.js</p>
       </li>
       <li>
-        <svg width={64} height={64}>
-          <use href={`${sprite}#mongodb`}></use>
-        </svg>
+        <a href="https://www.mongodb.com/">
+          <svg width={64} height={64}>
+            <use href={`${sprite}#mongodb`}></use>
+          </svg>
+        </a>
         <p className={css.techName}>MongoDB</p>
       </li>
       <li>
-        <svg width={64} height={64}>
-          <use href={`${sprite}#sass`}></use>
-        </svg>
+        <a href="https://sass-lang.com/">
+          <svg width={64} height={64}>
+            <use href={`${sprite}#sass`}></use>
+          </svg>
+        </a>
         <p className={css.techName}>Sass/Scss</p>
       </li>
       <li>
-        <svg width={75} height={65}>
-          <use href={`${sprite}#tailwindcss`}></use>
-        </svg>
+        <a href="https://tailwindcss.com/">
+          <svg width={75} height={65}>
+            <use href={`${sprite}#tailwindcss`}></use>
+          </svg>
+        </a>
         <p className={css.techName}>Tailwindcss</p>
       </li>
       <li>
-        <svg width={55} height={64}>
-          <use href={`${sprite}#figma`}></use>
-        </svg>
+        <a href="https://www.figma.com/">
+          <svg width={55} height={64}>
+            <use href={`${sprite}#figma`}></use>
+          </svg>
+        </a>
         <p className={css.techName}>Figma</p>
       </li>
       <li>
-        <svg width={64} height={64}>
-          <use href={`${sprite}#git`}></use>
-        </svg>
+        <a href="https://git-scm.com/">
+          <svg width={64} height={64}>
+            <use href={`${sprite}#git`}></use>
+          </svg>
+        </a>
         <p className={css.techName}>Git</p>
       </li>
     </ul>
