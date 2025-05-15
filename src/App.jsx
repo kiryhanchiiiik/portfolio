1a9fc1c5a74803a35c36ad82de88cdaa08ecf581
kiryhanchiiiik <162802442+kiryhanchiiiik@users.noreply.github.com>
2025-05-15 14:28:09 +0300

    add custom scrollbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Toaster } from "react-hot-toast";
+import SimpleBarReact from "simplebar-react";
 import About from "./components/About/About";
 import Certification from "./components/Certification/Certification";
 import Contact from "./components/Contact/Contact";
@@ -8,20 +9,28 @@ import Header from "./components/Header/Header";
 import Skills from "./components/Skills/Skills";
 import Work from "./components/Work/Work";
 import css from "./App.module.scss";
+import "simplebar-react/dist/simplebar.min.css";
 
 function App() {
   return (
-    <div className={css.container} data-theme="light">
-      <Header />
-      <Description />
-      <About />
-      <Skills />
-      <Work />
-      <Certification />
-      <Contact />
-      <Toaster position="bottom-center" />
-      <Footer />
-    </div>
+    <SimpleBarReact
+      className={css.scrollbar}
+      style={{ maxHeight: "100vh" }}
+      autoHide={false}
+      forceVisible="y"
+    >
+      <div className={css.container} data-theme="light">
+        <Header />
+        <Description />
+        <About />
+        <Skills />
+        <Work />
+        <Certification />
+        <Contact />
+        <Toaster position="bottom-center" />
+        <Footer />
+      </div>
+    </SimpleBarReact>
   );
 }
 
