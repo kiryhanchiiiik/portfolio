55a145792023946c69ad3dbea2dbca6adc366ee6
kiryhanchiiiik <162802442+kiryhanchiiiik@users.noreply.github.com>
2025-05-30 20:06:50 +0300

    add project

--- src/components/ProjectList/ProjectList.jsx
+++ src/components/ProjectList/ProjectList.jsx
@@ -2,6 +2,7 @@ import css from "./ProjectList.module.scss";
 import rentalCar from "../../img/rentalCar.png";
 import waterTracker from "../../img/waterTracker.png";
 import phoneBook from "../../img/phoneBook.png";
+import learnLingo from "../../img/learn-lingo.png";
 import sprite from "../../img/sprite.svg";
 
 const ProjectList = () => {
@@ -131,6 +132,62 @@ const ProjectList = () => {
           <a
             className={css.link}
             href="https://goit-react-hw-08-three-orpin-47.vercel.app/"
+            target="_blank"
+          >
+            <svg width={24} height={24}>
+              <use href={`${sprite}#link`}></use>
+            </svg>
+          </a>
+        </div>
+      </li>
+      <li className={css.projListChangeItem}>
+        <div className={`${css.imgContainer} ${css.imgContainerReverse}`}>
+          <img className={css.img} src={learnLingo} alt="Photo" />
+        </div>
+        <div className={`${css.descContainer} ${css.descContainerReverse}`}>
+          <h4 className={css.descTitle}>Learn Lingo</h4>
+          <p className={css.descPh}>
+            Learn Lingo is a modern web application designed to help language
+            learners find and book lessons with qualified language teachers. The
+            platform allows users to browse through available teachers, filter
+            them by language, skill level, and price, mark favorites for easy
+            access, and book trial lessons directly through the app.
+          </p>
+          <ul className={css.techList}>
+            <li className={css.techListItem}>
+              <p>React</p>
+            </li>
+            <li className={css.techListItem}>
+              <p>TypeScript</p>
+            </li>
+            <li className={css.techListItem}>
+              <p>Redux Toolkit</p>
+            </li>
+            <li className={css.techListItem}>
+              <p>React Toastify</p>
+            </li>
+            <li className={css.techListItem}>
+              <p>SCSS</p>
+            </li>
+            <li className={css.techListItem}>
+              <p>Git</p>
+            </li>
+            <li className={css.techListItem}>
+              <p>Axios</p>
+            </li>
+            <li className={css.techListItem}>
+              <p>Firebase</p>
+            </li>
+            <li className={css.techListItem}>
+              <p>React Router DOM</p>
+            </li>
+            <li className={css.techListItem}>
+              <p>React Hook Form</p>
+            </li>
+          </ul>
+          <a
+            className={css.link}
+            href="https://learn-lingo-lyart.vercel.app/"
             target="_blank"
           >
             <svg width={24} height={24}>
